9923e13a654b552a92da018c0d104603147230e1
Kevin Ring <kevin@kotachrome.com>
2016-07-13 16:09:08 +1000

    Eliminate perl and jq dependency, make update-lga-filter run every build.

--- gulpfile.js
+++ gulpfile.js
@@ -30,13 +30,32 @@ gulp.task('release', ['render-datasource-templates',/*'make-editor-schema', 'val
 gulp.task('watch', ['watch-datasource-templates']);
 gulp.task('default', ['build']);
 
-gulp.task('list-ga-services', function(done) {
-    var exec = require('child_process').exec;
-    exec('./list_services.sh', function (err, stdout, stderr) {
-        if (stderr)
-            console.log(stderr);
-        done(err);
-    });
+gulp.task('list-ga-services', ['render-datasource-templates'], function() {
+    const catalog = require('./build/nm.json');
+
+    const urls = {};
+    function findGAUrls(list) {
+        for (var i = 0; i < list.length; ++i) {
+            const item = list[i];
+            const url = item.url;
+            if (url && url.indexOf('ga.gov.au') >= 0) {
+                urls[url] = true;
+            }
+
+            if (item.items) {
+                findGAUrls(item.items);
+            }
+        }
+    }
+
+    findGAUrls(catalog.catalog);
+
+    const urlList = Object.keys(urls);
+    urlList.sort();
+
+    fs.writeFileSync('./build/ga_services.txt',
+        'These Geoscience Australia services are currently being referenced in the catalog.\n\n' +
+        urlList.join('\n'));
 });
 
 // Generate new schema for editor, and copy it over whatever version came with editor.
@@ -98,7 +117,7 @@ gulp.task('validate', ['merge-datasources', 'make-validator-schema'], function()
 
     "name": "<%= name %>"
  */
-gulp.task('render-datasource-templates', function() {
+gulp.task('render-datasource-templates', ['update-lga-filter'], function() {
     var ejs = require('ejs');
     var JSON5 = require('json5');
     try {
--- list_services.sh
+++
@@ -1,7 +0,0 @@
-OUT="build/ga_services.txt"
-echo "These Geoscience Australia services are currently being referenced in the catalog." > $OUT
-echo "Report generated at `date`" >> $OUT
-echo >> $OUT
-node_modules/jq-cli-wrapper/jq-releases/jq . < build/nm.json | \
-grep '"url":.*ga.gov.au' | sort | uniq | \
-perl -pi -e 's/^.*http/http/g;' | perl -pi -e 's/\",$//g;' >> $OUT
