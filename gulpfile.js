7437f53fccdecabe4ec762aa63adcd8b4799cbe7
Joerg Roth <info@leylines.ch>
2016-09-26 11:53:35 +0200

    fix catalog

--- gulpfile.js
+++ gulpfile.js
@@ -117,7 +117,7 @@ gulp.task('validate', ['merge-datasources', 'make-validator-schema'], function()
 
     "name": "<%= name %>"
  */
-gulp.task('render-datasource-templates', ['update-lga-filter'], function() {
+gulp.task('render-datasource-templates', function() {
     var ejs = require('ejs');
     var JSON5 = require('json5');
     try {
