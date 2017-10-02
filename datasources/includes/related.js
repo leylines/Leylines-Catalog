85ad6cf044d06deeab148a4a26e7fb92e97ef4b1
Joerg Roth <info@leylines.ch>
2017-10-02 11:58:43 +0200

    add related

---
+++ datasources/includes/related.js
@@ -0,0 +1,14 @@
+     {
+	"name": "Related, Donators, Sponsors",
+	"description": "People, Websites and Companies supporting me in a way",
+	"type": "group",
+	"items": [
+        {
+	   {
+		"name": "Related Pages",
+		"description": "Pages related to Leylines",
+		"type": "czml",
+		"url": "api/points/leylines/view_poi/Military"
+	   }
+	]
+     },
